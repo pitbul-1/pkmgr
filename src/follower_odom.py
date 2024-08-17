1a7060ce0294e86aef4727ed3f6507b83733fe5f
pitbul-1 <pakusznier@gmail.com>
2024-08-17 22:38:45 +0200

    addet theta to odom.csv

--- src/follower_odom.py
+++ src/follower_odom.py
@@ -3,6 +3,7 @@
 import rospy
 from nav_msgs.msg import Odometry
 import csv
+import tf.transformations as tf
 
 class FollowerPathRecorder:
     def __init__(self):
@@ -13,15 +14,21 @@ class FollowerPathRecorder:
         self.follower_file = open('follower_path.csv', 'w')
         self.follower_writer = csv.writer(self.follower_file)
         
-        self.follower_writer.writerow(['time', 'x', 'y', 'z'])
+        self.follower_writer.writerow(['time', 'x', 'y', 'z', 'theta'])
 
     def follower_callback(self, data):
         x = data.pose.pose.position.x
         y = data.pose.pose.position.y
         z = data.pose.pose.position.z
+        orientation_q = data.pose.pose.orientation
         timestamp = data.header.stamp.to_sec()
-        self.follower_writer.writerow([timestamp, x, y, z])
-        self.follower_path.append((timestamp, x, y, z))
+
+        # Convert quaternion to Euler angles
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        _, _, theta = tf.euler_from_quaternion(orientation_list)
+
+        self.follower_writer.writerow([timestamp, x, y, z, theta])
+        self.follower_path.append((timestamp, x, y, z, theta))
 
     def shutdown(self):
         self.follower_file.close()
--- src/leader_odom.py
+++ src/leader_odom.py
@@ -3,28 +3,35 @@
 import rospy
 from nav_msgs.msg import Odometry
 import csv
+import tf.transformations as tf
 
 class LeaderPathRecorder:
     def __init__(self):
-        self.leader_path = []
+        self.follower_path = []
         
-        self.leader_sub = rospy.Subscriber('/odom', Odometry, self.leader_callback)
+        self.follower_sub = rospy.Subscriber('/odom', Odometry, self.follower_callback)
         
-        self.leader_file = open('leader_path.csv', 'w')
-        self.leader_writer = csv.writer(self.leader_file)
+        self.follower_file = open('leader_path.csv', 'w')
+        self.follower_writer = csv.writer(self.follower_file)
         
-        self.leader_writer.writerow(['time', 'x', 'y', 'z'])
+        self.follower_writer.writerow(['time', 'x', 'y', 'z', 'theta'])
 
-    def leader_callback(self, data):
+    def follower_callback(self, data):
         x = data.pose.pose.position.x
         y = data.pose.pose.position.y
         z = data.pose.pose.position.z
+        orientation_q = data.pose.pose.orientation
         timestamp = data.header.stamp.to_sec()
-        self.leader_writer.writerow([timestamp, x, y, z])
-        self.leader_path.append((timestamp, x, y, z))
+
+        # Convert quaternion to Euler angles
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        _, _, theta = tf.euler_from_quaternion(orientation_list)
+
+        self.follower_writer.writerow([timestamp, x, y, z, theta])
+        self.follower_path.append((timestamp, x, y, z, theta))
 
     def shutdown(self):
-        self.leader_file.close()
+        self.follower_file.close()
 
 if __name__ == '__main__':
     rospy.init_node('leader_path_recorder', anonymous=True)
