c22da0a92bd484b2bdd00bc448c4c69bda56c718
pitbul-1 <pakusznier@gmail.com>
2024-08-27 11:38:41 +0200

    refactor: Update XboxTeleop class to adjust angular speed for improved control

--- src/leader_pad.py
+++ src/leader_pad.py
@@ -19,11 +19,9 @@ class XboxTeleop:
 
         # Linear and angular speed multipliers
         self.linear_speed = 0.2
-<<<<<<< HEAD
+
         self.angular_speed = 1.6
-=======
-        self.angular_speed = 0.12
->>>>>>> f2e8249bd2b7c07f282c14f0c778e84ad1a95b4b
+
 
     def joy_callback(self, data):
         self.twist.linear.x = self.linear_speed * data.axes[self.axis_linear]
